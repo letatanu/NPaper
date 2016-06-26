a8c9ce35b9163266df957e9112ec535d2f1e2d36
Me <letatanu@hotmail.com>
2016-06-25 17:20:12 -0700

    Initial Commit

---
+++ NPaper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NPaper
+//
+//  Created by LE  Nhut on 6/25/16.
+//  Copyright © 2016 LE  Nhut. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
